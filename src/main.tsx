e423c17e2de457b806fb0e483ceb4c364625df8f
Mihail <Misha200417@gmail.com>
2025-06-03 02:36:03 +0300

    edited main.tsx with all new pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,19 @@
+import { MantineProvider } from '@mantine/core';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Router';
+import { Notifications } from '@mantine/notifications';
+import { ColorSchemeToggle } from './components/ColorShemeToggle';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider defaultColorScheme='dark'>
+      <ColorSchemeToggle/>
+      <Notifications position="top-right" />
+      <RouterProvider router={router} />
+    </MantineProvider>
+  </React.StrictMode>
+);
