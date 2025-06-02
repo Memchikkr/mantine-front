0cdfdfee658f3e7869327040fa46f111f90bdad0
Mihail <Misha200417@gmail.com>
2025-06-03 02:28:23 +0300

    added for testing app server local config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  server: {
+    port: 80,
+    host: "127.0.0.1",
+  },
+  resolve: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
