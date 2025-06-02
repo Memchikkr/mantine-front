918c50f82fe0ed601aee2a15ff5596ece8855847
Mihail <Misha200417@gmail.com>
2025-06-03 02:35:43 +0300

    added router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,31 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HomePage } from './pages/Home.page';
+import { createBrowserRouter } from 'react-router-dom';
+import { AuthPage } from './pages/Auth.Page';
+import ProfilePage from './pages/Profile.Page';
+import { AppLayout } from './AppLayout';
+import { VehiclesPage } from './pages/Vehicles.Page';
+import RoutesPage from './pages/Routes.Page';
+import TripsPage from './pages/Trips.Page';
+import SearchTripsPage from './pages/SearchTrips.Page';
+import BookingsPage from './pages/Booking.Page';
+import TripBookingsPage from './pages/TripBooking.Page';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        children: [
+            { index: true, element: <AuthPage /> },
+        ],
+    },
+    {
+        element: <AppLayout />,
+        children: [
+          { path: "/profile", element: <ProfilePage /> },
+          { path: "/vehicles", element: <VehiclesPage /> },
+          { path: "/routes", element: <RoutesPage /> },
+          { path: "/trips", element: <TripsPage /> },
+          { path: "/search/trips", element: <SearchTripsPage /> },
+          { path: "/bookings", element: <BookingsPage /> },
+          { path: "/trips/:id/bookings", element: <TripBookingsPage /> },
+        ],
+      },
 ]);
-
-export function Router() {
-  return <RouterProvider router={router} />;
-}
